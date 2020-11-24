48adc8448238e6cd09301531eb17fb0735798693
vagrant <vagrant@vagrant.vm>
2020-11-24 20:26:05 +0000

    asdf

--- test.py
+++ test.py
@@ -11,12 +11,6 @@ class TestHello(unittest.TestCase):
     def test_index(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Axnet IoT Temperature API')
-
-    def test_recent_temps(self):
-        rv = self.app.get('/recent_temps/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Recent Bed Room Temps')
 
 if __name__ == '__main__':
     unittest.main()
