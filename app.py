37dd3c96eedd0e8e344268cfe40be2348617f375
Joe Axberg <joeaxberg@gmail.com>
2021-02-12 21:59:34 +0000

    lots of updates

--- app.py
+++ app.py
@@ -5,20 +5,34 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-#mongo
-app.config["MONGO_URI"] = "mongodb://192.168.80.100:27017/temperature"
+#creates a connection to the MongoDB.
+#make sure you have the correct URL.  If the MongoDB and Flask
+#are not on the same server, you will need to put an IP address
+#instead of localhost
+app.config["MONGO_URI"] = "mongodb://localhost:27017/temperature"
 mongo = PyMongo(app)
 
-@app.route('/')
-@app.route('/index/')
+#create the "index" or "root" level of the website.
+#this is the "home page"
+@app.route('/')        #the root route, for example: 192.168.56.108/
+@app.route('/index/')  #the root route, for example: 192.168.56.108/
 def index():
-    return render_template('index.html')
+    #when a user browses the root of the site, Flask returns a "template"
+    #html file name index.html.  This page is a static page.
+    #look for it under the "templates" directory
+    return render_template('index.html') 
+
+#define another route.  Here Flask will query the MongoDB, grab the most recent
+#temperature reading and return it as JSON back to the user.
+#this is not intended to be used by a person.  Instead this is an API that another
+#system for example would use to query the database.
 @app.route("/get_one_temp_api")
 def temp1():
     one_temp = mongo.db.temperature.find_one()
-    #print(temp_reading)
     return str(one_temp)
 
+#same as above, just returns the most recent 10 temperatures in the database as 
+#JSON objects.
 @app.route("/get_ten_temps_api")
 def temp10():
     temps = ""
@@ -28,6 +42,8 @@ def temp10():
         print(temps)
     return temps
 
+#this is the site's one human readable page.
+#queries the db and returns a table of 10 temps.
 @app.route("/recent_temps")
 def recent():
     temps = mongo.db.temperature.find().limit(10)
--- test.py
+++ test.py
@@ -13,6 +13,28 @@ class TestHello(unittest.TestCase):
         self.assertEqual(rv.status, '200 OK')
 
     #add two more tests please
+    #duplicate the class above blah blah
+
+class TestGetOne(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_index(self):
+        rv = self.app.get('/get_one_temp_api')
+        self.assertEqual(rv.status, '200 OK')
+
+class Test404(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_index(self):
+        rv = self.app.get('/idontexist')
+        self.assertEqual(rv.status, '404 NOT FOUND')
+
 
 if __name__ == '__main__':
     unittest.main()
